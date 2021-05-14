ac17b8753fdf4153573420c66f5dfbcc69d4abee
Barry Song <524000659@qq.com>
2021-05-14 17:27:23 +0800

    style(loginbox): a sketch for loginbox layout

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,15 +1,9 @@
 import React, { FC } from 'react';
 import styles from './index.less';
 import { CreditCard } from '@/assets/login/index';
-import { Button, Checkbox, Form, Input } from 'antd';
-
-const layout = {
-  labelCol: { span: 8 },
-  wrapperCol: { span: 16 },
-};
-const tailLayout = {
-  wrapperCol: { offset: 8, span: 16 },
-};
+import { Button, Checkbox, Divider, Form, Input, Typography } from 'antd';
+import { WechatOutlined } from '@ant-design/icons';
+const { Title, Paragraph } = Typography;
 const Login: FC = () => {
   const onFinish = (values: any) => {
     console.log('Success:', values);
@@ -22,46 +16,73 @@ const Login: FC = () => {
   return (
     <div className={styles.PageContainer}>
       <div className={styles.loginBox}>
-        <div className={styles.loginForm}>
-          <div className={styles.title}>登 录</div>
+        <div className={styles.formContainer}>
+          <div>
+            <Title>登录</Title>
+            <Paragraph>更好，更高效的记账软件</Paragraph>
+          </div>
+
+          <div className={styles.wechat}>
+            <Button
+              className={styles.wecharBtn}
+              shape="round"
+              icon={<WechatOutlined style={{ color: 'green' }} />}
+              size="large"
+            >
+              微信登录
+            </Button>
+          </div>
+          <Divider orientation="center">使用邮箱登录</Divider>
           <Form
-            {...layout}
+            layout="vertical"
             name="basic"
+            className={styles.form}
             initialValues={{ remember: true }}
             onFinish={onFinish}
             onFinishFailed={onFinishFailed}
           >
             <Form.Item
-              label="Username"
-              name="username"
-              rules={[
-                { required: true, message: 'Please input your username!' },
-              ]}
+              label="邮箱"
+              name="email"
+              rules={[{ required: true, message: '请输入用户名!' }]}
             >
-              <Input />
+              <Input
+                size="large"
+                placeholder="xxxx@website.com"
+                className={styles.inputItem}
+              />
             </Form.Item>
 
             <Form.Item
-              label="Password"
+              label="密码"
               name="password"
-              rules={[
-                { required: true, message: 'Please input your password!' },
-              ]}
+              rules={[{ required: true, message: '请输入密码！' }]}
             >
-              <Input.Password />
+              <Input.Password size="large" className={styles.inputItem} />
             </Form.Item>
 
-            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-              <Checkbox>Remember me</Checkbox>
+            <Form.Item>
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>记住我</Checkbox>
+              </Form.Item>
+
+              <a className={styles.loginFormForgot} href="">
+                忘记密码
+              </a>
             </Form.Item>
 
-            <Form.Item {...tailLayout}>
-              <Button type="primary" htmlType="submit">
-                Submit
+            <Form.Item style={{ marginBottom: 0 }}>
+              <Button
+                className={styles.loginButton}
+                type="primary"
+                size="large"
+                shape="round"
+                htmlType="submit"
+              >
+                登录
               </Button>
             </Form.Item>
           </Form>
-          <div className={styles.wechat}></div>
         </div>
         <div className={styles.loginImgContainer}>
           <CreditCard className={styles.loginImg} height={500} width={500} />
