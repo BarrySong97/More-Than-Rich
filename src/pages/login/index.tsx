8e8d14f90476f9a5a00dbd5d4d5f835778efeb53
Barry Song <524000659@qq.com>
2021-05-18 13:26:25 +0800

    feat(login): seperator login components

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,92 +1,26 @@
 import React, { FC, useEffect, useState } from 'react';
 import styles from './index.less';
-import { CreditCard } from '@/assets/login/index';
-import { Button, Checkbox, Divider, Form, Input, Typography } from 'antd';
-import { WechatOutlined } from '@ant-design/icons';
-const { Title, Paragraph } = Typography;
+import LoginForm from './components/login-form';
+import RegisterForm from './components/register-form';
+import { Transaction, Wallet } from '@/assets/login/index';
+import { Carousel } from 'antd';
+
 const Login: FC = () => {
-  const onFinish = (values: any) => {
-    console.log('Success:', values);
-  };
-
-  const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
-  };
-
+  const [state, setState] = useState<'login' | 'register'>('login');
   return (
     <div className={styles.PageContainer}>
       <div className={styles.loginBox}>
-        <div className={styles.formContainer}>
-          <div style={{ width: '60%' }}>
-            <div>
-              <Title>登录</Title>
-              <Paragraph>更好，更高效的记账软件</Paragraph>
-            </div>
-            <div className={styles.wechat}>
-              <Button
-                className={styles.wecharBtn}
-                shape="round"
-                icon={<WechatOutlined style={{ color: 'green' }} />}
-                size="large"
-              >
-                微信登录
-              </Button>
-            </div>
-            <Divider orientation="center">使用邮箱登录</Divider>
-            <Form
-              layout="vertical"
-              name="basic"
-              className={styles.form}
-              initialValues={{ remember: true }}
-              onFinish={onFinish}
-              onFinishFailed={onFinishFailed}
-            >
-              <Form.Item
-                label="邮箱"
-                name="email"
-                rules={[{ required: true, message: '请输入用户名!' }]}
-              >
-                <Input
-                  size="large"
-                  placeholder="xxxx@website.com"
-                  className={styles.inputItem}
-                />
-              </Form.Item>
-
-              <Form.Item
-                label="密码"
-                name="password"
-                rules={[{ required: true, message: '请输入密码！' }]}
-              >
-                <Input.Password size="large" className={styles.inputItem} />
-              </Form.Item>
-
-              <Form.Item>
-                <Form.Item name="remember" valuePropName="checked" noStyle>
-                  <Checkbox>记住我</Checkbox>
-                </Form.Item>
-
-                <a className={styles.loginFormForgot} href="">
-                  忘记密码
-                </a>
-              </Form.Item>
-
-              <Form.Item style={{ marginBottom: 0 }}>
-                <Button
-                  className={styles.loginButton}
-                  type="primary"
-                  size="large"
-                  shape="round"
-                  htmlType="submit"
-                >
-                  登录
-                </Button>
-              </Form.Item>
-            </Form>
-          </div>
-        </div>
+        {state === 'login' ? <LoginForm /> : <RegisterForm />}
+        <div>123</div>
         <div className={styles.loginImgContainer}>
-          <CreditCard className={styles.loginImg} height={500} width={500} />
+          <Carousel autoplay>
+            <div>
+              <Transaction className={styles.loginImg} />
+            </div>
+            <div>
+              <Wallet className={styles.loginImg} />
+            </div>
+          </Carousel>
         </div>
       </div>
     </div>
